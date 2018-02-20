f1b7cb894cfea3baf63376030c1dde3032f4581d
imacrostat <35893348+imacrostat@users.noreply.github.com>
2018-02-20 13:36:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-﻿
-var eltBurger, eltSidebar;
+
+var eltBurger = document.querySelector("#burger");
+var eltSidebar = document.querySelector("#sidebar");
+
+var options = {
+	backgroundColor: getComputedStyle(document.body).backgroundColor,
+	transitionDuration: getComputedStyle(eltSidebar).transitionDuration,
+	fontSize: getComputedStyle(document.querySelector("p")).fontSize,
+	fontFamily: getComputedStyle(document.body).fontFamily,
+	fonts: ["Arial", "Courier New", "Georgia", "Lucida Sans Unicode", "Tahoma"]
+};
+
+
+/* --------- sidebar initialization & animation --------- */
+function initializeSidebar() {
+	document.querySelector("#fontsize").value = parseInt(options.fontSize);
+	document.querySelector("#bgcolor").value = toHexColor(options.backgroundColor);
+	document.querySelector("#remover").disabled = false;
+	
+	setBurgerArrow();
+	insertRadioInput();
+	
+	function toHexColor(c) {
+		c = c.match(/(\d+), (\d+), (\d+)/);
+		return ("#" + (+c[1]).toString(16) + (+c[2]).toString(16) + (+c[3]).toString(16));
+	}
+	
+	function insertRadioInput() {
+		var htmlRadio = "", arrFonts = options.fonts;
+		
+		// add the default font family into the array and sort it
+		var defFont = options.fontFamily.replace(/"/g, "'");
+		arrFonts.push(defFont);
+		
+		for (i in arrFonts) {
+			if (arrFonts[i].indexOf("'") < 0) {
+				arrFonts[i] = "'" + arrFonts[i] + "'";
+			}
+		}
+		arrFonts.sort();
+
+		// build an html code for the radio control
+		for (i in arrFonts) {
+			var c = (arrFonts[i].replace(/'/g, "") == defFont.replace(/'/g, "")) ? " checked" : ""; 
+			htmlRadio += 
+				'<input type="radio" name="fontfamily" value="' + arrFonts[i] + '" onchange="changeFontFamily()"' + c + '></input>' +
+				'<span style="font-family: ' + arrFonts[i] + '">' + arrFonts[i].split(" ")[0].replace(/'/g, "") + '</span><br>\n';
+		}
+
+		window.document.querySelector("#controls > div:nth-last-child(2) > label").insertAdjacentHTML("afterEnd", htmlRadio);
+	}
+}
 
 function toggleSidebar() {
 	eltSidebar.classList.toggle("folded");
@@ -10,20 +60,28 @@ function toggleSidebar() {
 
 function setBurgerArrow() {
 	const tUp = "∧", tRt = ">", tDn = "∨", tLt = "<";
+	// adjust for a narrow width
 	var m = document.documentElement.clientWidth < 460;
 	eltBurger.innerHTML = (eltSidebar.classList.contains("folded")) ? (m ? tDn : tRt) : (m ? tUp : tLt);
 }
 
-function getHiddenTimeout(sidebarTimeout) {
-	var transDuration = parseFloat(getComputedStyle(document.querySelector("#sidebar")).transitionDuration) * 1000;
-	return sidebarTimeout ? !(eltSidebar.classList.contains("folded")) * transDuration : transDuration;
+function getHiddenTimeout(isControlsTimeout) {
+	var transDuration = parseFloat(options.transitionDuration) * 1000;
+	return isControlsTimeout ? !(eltSidebar.classList.contains("folded")) * transDuration: transDuration * 0.9;
 }
 
 
+/* --------- content style customization --------- */
 function changeFontSize() {
 	var eltFontSize = document.querySelector("#fontsize");
-	var fontSize = +eltFontSize.value;
+	var fontSize = eltFontSize.value;
 	
+	if (!fontSize.trim().match(/^\d{1,2}$/)) {
+		// default message displayed
+		return;
+	}
+	
+	fontSize = +fontSize;
 	if (fontSize < +eltFontSize.min || fontSize > +eltFontSize.max) {
 		alert("The font size should lie between " + eltFontSize.min + " and " + eltFontSize.max + "!");
 		return;
@@ -35,11 +93,9 @@ function changeFontSize() {
 	}
 }
 
-
-function changeBGColor() {
+function changeBackgroundColor() {
 	document.querySelector("div.content").style.backgroundColor = document.querySelector("#bgcolor").value;
 }
-
 
 function changeFontFamily() {
 	var elsRadio = document.getElementsByName("fontfamily");
@@ -52,56 +108,15 @@ function changeFontFamily() {
 	}
 }
 
-
 function removeLastP() {
 	document.querySelector("div.content > p:last-of-type").remove();
 	
 	if (!document.querySelector("div.content > p:last-of-type")) {
-		document.querySelector("#remover").remove();
+		document.querySelector("#remover").disabled = true;
 	}
 }
 
 
-window.onload = function() {
-	eltBurger = document.querySelector("#burger");	
-	eltSidebar = document.querySelector("#sidebar");
-		
-	document.querySelector("#fontsize").value = parseInt(getComputedStyle(document.querySelector("p")).fontSize);
-	document.querySelector("#bgcolor").value = toHexColor(getComputedStyle(document.body).backgroundColor);
-	
-	setBurgerArrow();
-	appendRadioInput();
-}
-
-function toHexColor(c) {
-	c = c.match(/(\d+), (\d+), (\d+)/);
-	return ("#" + (+c[1]).toString(16) + (+c[2]).toString(16) + (+c[3]).toString(16));
-}
-
-function appendRadioInput() {
-	var htmlRadio = "";
-	var arrFonts = ["Arial", "Courier New", "Georgia", "Lucida Sans Unicode", "Tahoma"];
-	
-	var defFont = getComputedStyle(document.body).fontFamily.replace(/"/g, "'");
-	arrFonts.push(defFont);
-	
-	for (i in arrFonts) {
-		if (arrFonts[i].indexOf("'") < 0) {
-			arrFonts[i] = "'" + arrFonts[i] + "'";
-		}
-	}
-	arrFonts.sort();
-
-	for (i in arrFonts) {
-		var c = (arrFonts[i].replace(/'/g, "") == defFont.replace(/'/g, "")) ? " checked" : ""; 
-		htmlRadio += '<input type="radio" name="fontfamily" value="' + arrFonts[i] + '" onchange="changeFontFamily()"' + c + '></input><span style="font-family: ' + arrFonts[i] + '">' + arrFonts[i].split(" ")[0].replace(/'/g, "") + '</span><br>\n';
-	}
-
-	window.document.querySelector("#controls > div:last-child > label").insertAdjacentHTML("afterEnd", htmlRadio);
-}
-
-
-window.onresize = function() {
-	setBurgerArrow();
-}
-
+/* --------- window functions --------- */
+window.onload = initializeSidebar;
+window.onresize = setBurgerArrow;
